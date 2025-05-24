dd045f2e702e9c1eb985e52836a07a0efc9dabc4
Longin <l.khibovskiy@gmail.com>
2025-05-24 17:38:50 +0300

    add header

--- templates/header.php
+++ templates/header.php
@@ -1,1 +1,78 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TyeMood</title>
+    <link rel="icon" href="/images/logo.png">
+    <link rel="stylesheet" href="/css/index.css">
+    <script src="/js/app.js" defer></script>
+</head>
+<body>
+<header>
+    <div class="header_container">
+        <div class="header_logo_container">
+            <a class="header_consultation_container" href="/">
+                <p class="navigation_semibold">Получить консультацию</p>
+                <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.32391 0.425432C3.1844 0.416572 3.04553 0.435714 2.91547 0.481733C2.7854 0.527752 2.66674 0.599722 2.56645 0.693427C2.46616 0.787132 2.38625 0.900684 2.33141 1.02743C2.27656 1.15417 2.24789 1.29156 2.24708 1.43153C2.24626 1.5715 2.27331 1.71125 2.32664 1.84257C2.37997 1.9739 2.45851 2.09416 2.55766 2.19631C2.65681 2.29846 2.77456 2.38044 2.90403 2.43745C3.03349 2.49446 3.17206 2.52535 3.31161 2.5283L13.3934 2.95969L0.391813 14.6663C0.188561 14.8494 0.0690388 15.1086 0.0595401 15.3871C0.0500429 15.6656 0.151347 15.9404 0.341166 16.1513C0.530985 16.3621 0.793769 16.4916 1.07171 16.5112C1.34965 16.5309 1.61999 16.4391 1.82324 16.2561L14.8268 4.54761L14.2019 14.6232C14.1903 14.7623 14.2065 14.9033 14.2497 15.038C14.2928 15.1728 14.362 15.2984 14.4533 15.4077C14.5445 15.517 14.6559 15.6077 14.7809 15.6745C14.9059 15.7412 15.0421 15.7827 15.1814 15.7965C15.3207 15.8104 15.4603 15.7962 15.5921 15.7549C15.7239 15.7136 15.8452 15.646 15.9488 15.5561C16.0525 15.4661 16.1365 15.3556 16.1959 15.2311C16.2552 15.1065 16.2888 14.9704 16.2945 14.8308L17.06 2.4927C17.0791 2.1864 16.997 1.87864 16.8265 1.61681C16.7473 1.48054 16.6397 1.36246 16.5114 1.27103C16.2707 1.07835 15.977 0.967035 15.6744 0.95381L3.32391 0.425432Z"
+                          fill="#2A2A2C"/>
+                </svg>
+            </a>
+            <a href="/" class="header_logo">
+                <img src="/images/logo.png" alt="" height="80px" width="105px">
+                <h3 class="header_title">TyeMood</h3>
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                    <path d="M6.00132 12C9.31378 12 12 9.31371 12 5.99999C12 2.68629 9.31378 0 6.00132 0C2.6862 0 0 2.68629 0 5.99999C0 9.31371 2.6862 12 6.00132 12Z"
+                          fill="#2A2A2C"/>
+                </svg>
+            </a>
+            <div class="header_icons_container">
+                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.3333 4C6.836 4 4 7.19059 4 11.1252C4 19 16 28 16 28C16 28 28 19 28 11.1252C28 6.25035 25.164 4 21.6667 4C19.1867 4 17.04 5.60376 16 7.93882C14.96 5.60376 12.8133 4 10.3333 4Z"
+                          stroke="#2A2A2C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.73463 20.775L4.2138 11.8C3.98391 10.4425 3.86896 9.765 4.2378 9.32C4.60538 8.875 5.28242 8.875 6.63525 8.875H25.3652C26.718 8.875 27.3951 8.875 27.7626 9.32C28.1315 9.765 28.0153 10.4425 27.7866 11.8L26.2658 20.775C25.7618 23.75 25.5104 25.2363 24.481 26.1188C23.4528 27 21.9698 27 19.004 27H12.9965C10.0306 27 8.54765 27 7.51945 26.1175C6.48999 25.2363 6.23862 23.7487 5.73463 20.7737M22.9475 8.875C22.9475 7.05164 22.2156 5.30295 20.9127 4.01364C19.6098 2.72433 17.8428 2 16.0002 2C14.1577 2 12.3906 2.72433 11.0877 4.01364C9.78486 5.30295 9.05291 7.05164 9.05291 8.875"
+                          stroke="#2A2A2C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+        </div>
+        <hr>
+        <div class="header_navigation_container">
+            <div class="header_navigation">
+                <a href="/" class="navigation_semibold">
+                    О нас
+                    <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6.20645 7.61705L8.00994 9.30896C8.07436 9.36952 8.15088 9.41756 8.23512 9.45035C8.31936 9.48313 8.40966 9.5 8.50086 9.5C8.59206 9.5 8.68236 9.48313 8.7666 9.45035C8.85083 9.41756 8.92735 9.36952 8.99177 9.30896L10.7953 7.61705C11.234 7.20551 10.9206 6.5 10.3009 6.5H6.69388C6.07414 6.5 5.76776 7.20551 6.20645 7.61705Z"
+                              fill="#2A2A2C"/>
+                    </svg>
+                </a>
+                <a href="/" class="navigation_semibold">Услуги</a>
+                <div class="dropdown-container">
+                    <div class="dropdown-toggle hover-dropdown navigation_semibold">
+                        Каталог
+                        <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.20645 7.61705L8.00994 9.30896C8.07436 9.36952 8.15088 9.41756 8.23512 9.45035C8.31936 9.48313 8.40966 9.5 8.50086 9.5C8.59206 9.5 8.68236 9.48313 8.7666 9.45035C8.85083 9.41756 8.92735 9.36952 8.99177 9.30896L10.7953 7.61705C11.234 7.20551 10.9206 6.5 10.3009 6.5H6.69388C6.07414 6.5 5.76776 7.20551 6.20645 7.61705Z"
+                                  fill="#2A2A2C"/>
+                        </svg>
+                    </div>
+                    <div class="dropdown-menu">
+                        <?php
+                        $result = $link->query('SELECT * FROM main_categories');
+                        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                        foreach ($rows as $row):
+                            ?>
+                            <a href="/" class="regular_04"><?= $row['name'] ?></a>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+                <a href="/" class="navigation_semibold">Портфолио</a>
+                <a href="/" class="navigation_semibold">FAQ</a>
+                <a href="/" class="navigation_semibold">Контакты</a>
+            </div>
+        </div>
+    </div>
+</header>
