4c5a86ae8764ce089d1549aa0c50b324f6cb071b
Longin <l.khibovskiy@gmail.com>
2025-05-24 15:23:44 +0300

    add fonts

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+require_once './settings/connect.php';
+
+$page = $_GET['page'] ?? 'index';
+$pagePath = 'pages/' . $page . '.php';
+include './templates/header.php';
+if (file_exists($pagePath)) {
+    include $pagePath;
+} else {
+    include 'pages/404.php';
+}
+
+include './templates/footer.php';
+?>
