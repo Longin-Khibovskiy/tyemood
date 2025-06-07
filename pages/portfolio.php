63c0598a1d2deba4ec646c4386c77bb7ae2d733e
Longin <l.khibovskiy@gmail.com>
2025-06-07 23:30:48 +0300

    add adaptive

--- pages/portfolio.php
+++ pages/portfolio.php
@@ -4,7 +4,7 @@
             <?php for ($i = 1; $i <= 3; $i++): ?>
                 <?php if ($i % 2): ?>
                     <div class="portfolio_row">
-                        <img src="/images/portfolio/img_<?= ($i - 1) * 5 ?>.png" alt="">
+                        <img src="/images/portfolio/img_<?= ($i - 1) * 5 ?>.png" alt="" class="portfolio_main_img">
                         <div class="portfolio_row_right">
                             <?php for ($j = (($i - 1) * 5) + 1; $j <= ($i * 5) - 1; $j++): ?>
                                 <img src="/images/portfolio/img_<?= $j ?>.png" alt="">
@@ -18,7 +18,7 @@
                                 <img src="/images/portfolio/img_<?= $j ?>.png" alt="">
                             <?php endfor; ?>
                         </div>
-                        <img src="/images/portfolio/img_<?= ($i * 5) - 1 ?>.png" alt="">
+                        <img src="/images/portfolio/img_<?= ($i * 5) - 1 ?>.png" alt="" class="portfolio_main_img">
                     </div>
                 <?php endif; ?>
             <?php endfor; ?>
